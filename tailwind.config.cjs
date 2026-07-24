b118cfaeeb4b6ae3dc907c0101bc1bd8a28329b9
Gandhi <gandhirahmawan@gmail.com>
2026-07-24 22:12:47 +0700

    Setup Tailwind CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.css',
+    './app/**/*.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

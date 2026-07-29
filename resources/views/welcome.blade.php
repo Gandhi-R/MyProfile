bb634b5a1638a8fd120cbf3c7a9337d413d93a52
Gandhi <gandhirahmawan@gmail.com>
2026-07-29 19:11:50 +0700

    feat: add initial home section layout (WIP)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,14 +12,44 @@
 
 <body class="bg-neo-white font-sans antialiased ">
 
-    <!-- Memanggil Komponen Navbar -->
     <x-navbar />
 
     <!-- Konten Website -->
 
     <main>
         <!-- Setiap section kita kasih ID buat navigasi dari Navbar -->
-        <section id="home" class="page-section section-home"></section>
+        <section id="home" class="page-section section-home">
+            <section id="home" class="page-section section-home">
+            <div class="home-container">
+                
+                <!-- Bagian Kiri (Teks & Tombol) -->
+                <div class="home-content">
+                    <div class="badge-hello">HELLO!! I, M</div>
+                    
+                    <h1 class="home-title">GANDHI RAHMAWAN</h1>
+                    
+                    <div class="badge-role">Cyber Security Enthusiast</div>
+                    
+                    <div class="home-buttons">
+                        <a href="#project" class="btn-work">VIEW ON MY WORK</a>
+                        <a href="#contact" class="btn-wa">
+                            <img src="{{ asset('images/icons/logo-whatsapp.svg') }}" alt="WA" class="icon-btn">
+                            CHAT ON WHATSAPP
+                        </a>
+                    </div>
+                </div>
+
+                <!-- Bagian Kanan (Foto Profile) -->
+                <div class="home-image-wrapper">
+                    <div class="profile-card">
+                        <img src="{{ asset('images/profile/profile-1.png') }}" alt="Gandhi Rahmawan" class="profile-img">
+                        <p class="profile-username">@gandirmn._</p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        </section>
         
         <section id="about" class="page-section section-about"></section>
         
@@ -32,6 +62,7 @@
         <section id="certificate" class="page-section section-certificate"></section>
         
         <section id="contact" class="page-section section-contact"></section>
+
     </main>
 
 </body>

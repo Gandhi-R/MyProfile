732074ad290c21a07156e5c6a783dc84fa05ca0f
Gandhi <gandhirahmawan@gmail.com>
2026-08-26 15:34:50 +0700

    progres home page responsive

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -3,24 +3,21 @@
 
                 <div class="home-content">
 
-                    <!-- 1. HELLOO and Garis Gantung -->
                     <div class="hello-wrapper">
                         <div class="hanging-line left-line"></div>
                         <div class="hanging-line right-line"></div>
                         <div class="badge-hello">HELLOO!! I,M</div>
                     </div>
 
-                    <!-- 2. Judul Nama & Kotak Dekorasi di Belakangnya -->
                     <div class="title-wrapper">
                         <div class="deco-box green-box"></div>
                         <div class="deco-box blue-box"></div>
                         <h1 class="home-title">GANDHI RAHMAWAN</h1>
                     </div>
 
-                    <!-- 3. Badge Role -->
                     <div class="badge-role">Cyber Security Enthusiast</div>
 
-                    <!-- 4. Tombol -->
+
                     <div class="home-buttons">
                         <a href="#project" class="btn-work">VIEW ON MY WORK</a>
                         <a href="#contact" class="btn-wa">
@@ -30,19 +27,6 @@
                     </div>
                 </div>
 
-                <!-- BAGIAN KANAN (Foto Profile Polaroid) -->
-                <div class="home-image-wrapper">
-                    <div class="profile-card">
-                        <!-- Stiker Bintang (Pakai SVG langsung biar tajam) -->
-                        <div class="star-sticker">
-                            <img src="{{ asset('images/icons/icon-star.png') }}" alt="Star">
-                        </div>
-
-                        <img src="{{ asset('images/profile/profile-1.png') }}" alt="Gandhi Rahmawan"
-                            class="profile-img">
-                        <p class="profile-username">@gandirmn._</p>
-                    </div>
-                </div>s
 
             </div>
         </section>
